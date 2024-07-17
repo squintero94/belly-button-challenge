93b91c1d128b34fc9d2c964d3576d2f078643de9
squintero94 <squintero94@gmail.com>
2024-07-17 17:58:48 -0400

    corrrected a mistake with function init

--- static/js/app.js
+++ static/js/app.js
@@ -102,7 +102,7 @@ function init() {
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
     names.forEach((name) => {
-      dropdown.append("option")
+      dataset.append("option")
         .text(name)
         .attr("value", name);
     });
